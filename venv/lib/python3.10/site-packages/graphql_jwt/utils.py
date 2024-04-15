cdd2e974779fb1edea5d4938d286c54b96399ae0
dennismbugua <dennis@starlaunch.org>
2024-04-15 16:00:36 +0300

    A few Updates

---
+++ venv/lib/python3.10/site-packages/graphql_jwt/utils.py
@@ -0,0 +1,97 @@
+from calendar import timegm
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext as _
+
+import jwt
+
+from .exceptions import GraphQLJWTError
+from .settings import jwt_settings
+
+
+def jwt_payload(user, context=None):
+    username = user.get_username()
+
+    if hasattr(username, 'pk'):
+        username = username.pk
+
+    payload = {
+        user.USERNAME_FIELD: username,
+        'exp': datetime.utcnow() + jwt_settings.JWT_EXPIRATION_DELTA,
+    }
+
+    if jwt_settings.JWT_ALLOW_REFRESH:
+        payload['orig_iat'] = timegm(datetime.utcnow().utctimetuple())
+
+    if jwt_settings.JWT_AUDIENCE is not None:
+        payload['aud'] = jwt_settings.JWT_AUDIENCE
+
+    if jwt_settings.JWT_ISSUER is not None:
+        payload['iss'] = jwt_settings.JWT_ISSUER
+
+    return payload
+
+
+def jwt_encode(payload, context=None):
+    return jwt.encode(
+        payload,
+        jwt_settings.JWT_SECRET_KEY,
+        jwt_settings.JWT_ALGORITHM,
+    ).decode('utf-8')
+
+
+def jwt_decode(token, context=None):
+    return jwt.decode(
+        token,
+        jwt_settings.JWT_SECRET_KEY,
+        jwt_settings.JWT_VERIFY,
+        options={
+            'verify_exp': jwt_settings.JWT_VERIFY_EXPIRATION,
+        },
+        leeway=jwt_settings.JWT_LEEWAY,
+        audience=jwt_settings.JWT_AUDIENCE,
+        issuer=jwt_settings.JWT_ISSUER,
+        algorithms=[jwt_settings.JWT_ALGORITHM])
+
+
+def get_authorization_header(request):
+    auth = request.META.get('HTTP_AUTHORIZATION', '').split()
+    prefix = jwt_settings.JWT_AUTH_HEADER_PREFIX
+
+    if len(auth) != 2 or auth[0].lower() != prefix.lower():
+        return None
+    return auth[1]
+
+
+def get_payload(token, context=None):
+    try:
+        payload = jwt_settings.JWT_DECODE_HANDLER(token, context)
+    except jwt.ExpiredSignature:
+        raise GraphQLJWTError(_('Signature has expired'))
+    except jwt.DecodeError:
+        raise GraphQLJWTError(_('Error decoding signature'))
+    except jwt.InvalidTokenError:
+        raise GraphQLJWTError(_('Invalid token'))
+    return payload
+
+
+def get_user_by_natural_key(user_id):
+    User = get_user_model()
+    try:
+        return User.objects.get_by_natural_key(user_id)
+    except User.DoesNotExist:
+        return None
+
+
+def get_user_by_payload(payload):
+    username = payload.get(get_user_model().USERNAME_FIELD)
+
+    if not username:
+        raise GraphQLJWTError(_('Invalid payload'))
+
+    user = get_user_by_natural_key(username)
+
+    if user is not None and not user.is_active:
+        raise GraphQLJWTError(_('User is disabled'))
+    return user
---
+++ venv/lib/python3.10/site-packages/safedelete/tests/test_soft_delete_cascade.py
@@ -0,0 +1,100 @@
+from django.db import models
+from django.test import TestCase
+from safedelete import SOFT_DELETE_CASCADE
+from safedelete.models import SafeDeleteModel
+from safedelete.tests.models import Article, Author, Category
+
+
+class Press(SafeDeleteModel):
+    name = models.CharField(max_length=200)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+
+class PressNormalModel(models.Model):
+    name = models.CharField(max_length=200)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+
+class CustomAbstractModel(SafeDeleteModel):
+
+    class Meta:
+        abstract = True
+
+
+class ArticleView(CustomAbstractModel):
+    _safedelete_policy = SOFT_DELETE_CASCADE
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+
+class SimpleTest(TestCase):
+
+    def setUp(self):
+
+        self.authors = (
+            Author.objects.create(),
+            Author.objects.create(),
+            Author.objects.create(),
+        )
+
+        self.categories = (
+            Category.objects.create(name='category 0'),
+            Category.objects.create(name='category 1'),
+            Category.objects.create(name='category 2'),
+        )
+
+        self.articles = (
+            Article.objects.create(author=self.authors[1]),
+            Article.objects.create(author=self.authors[1], category=self.categories[1]),
+            Article.objects.create(author=self.authors[2], category=self.categories[2]),
+        )
+
+        self.press = (
+            Press.objects.create(name='press 0', article=self.articles[2])
+        )
+
+    def test_soft_delete_cascade(self):
+        self.assertEqual(Author.objects.count(), 3)
+        self.assertEqual(Article.objects.count(), 3)
+        self.assertEqual(Category.objects.count(), 3)
+        self.assertEqual(Press.objects.count(), 1)
+
+        self.authors[2].delete(force_policy=SOFT_DELETE_CASCADE)
+
+        self.assertEqual(Author.objects.count(), 2)
+        self.assertEqual(Author.all_objects.count(), 3)
+        self.assertEqual(Article.objects.count(), 2)
+        self.assertEqual(Article.all_objects.count(), 3)
+        self.assertEqual(Press.objects.count(), 0)
+        self.assertEqual(Press.all_objects.count(), 1)
+
+    def test_soft_delete_cascade_with_normal_model(self):
+        PressNormalModel.objects.create(name='press 0', article=self.articles[2])
+        self.authors[2].delete(force_policy=SOFT_DELETE_CASCADE)
+
+        self.assertEqual(Author.objects.count(), 2)
+        self.assertEqual(Author.all_objects.count(), 3)
+        self.assertEqual(Article.objects.count(), 2)
+        self.assertEqual(Article.all_objects.count(), 3)
+        self.assertEqual(Press.objects.count(), 0)
+        self.assertEqual(Press.all_objects.count(), 1)
+
+    def test_soft_delete_cascade_with_abstract_model(self):
+        ArticleView.objects.create(article=self.articles[2])
+
+        self.articles[2].delete(force_policy=SOFT_DELETE_CASCADE)
+
+        self.assertEqual(Article.objects.count(), 2)
+        self.assertEqual(Article.all_objects.count(), 3)
+
+        self.assertEqual(ArticleView.objects.count(), 0)
+        self.assertEqual(ArticleView.all_objects.count(), 1)
+
+    def test_undelete_with_soft_delete_cascade_policy(self):
+        self.authors[2].delete(force_policy=SOFT_DELETE_CASCADE)
+        self.authors[2].undelete(force_policy=SOFT_DELETE_CASCADE)
+
+        self.assertEqual(Author.objects.count(), 3)
+        self.assertEqual(Article.objects.count(), 3)
+        self.assertEqual(Category.objects.count(), 3)
+        self.assertEqual(Press.objects.count(), 1)
